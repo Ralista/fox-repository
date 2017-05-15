aedabf27aca7cb5b58ba132e88f8844f7feca35e
Joanna <ralistochka@gmail.com>
2017-05-15 22:50:17 +0500

    Задание 1

---
+++ fox/src/test/java/ru/fox/Test1.java
@@ -0,0 +1,27 @@
+package ru.fox;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+    private WebDriver driver;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test1(){
+        driver.get("http://www.google.com/");
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
