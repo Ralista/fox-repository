511c4359aa11d42fdde30393c80944fd5b02ac7e
Joanna <ralistochka@gmail.com>
2017-05-25 11:07:36 +0500

    Задание 5

---
+++ fox/src/test/java/ru/fox/TestFirefoxESR.java
@@ -0,0 +1,45 @@
+package ru.fox;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+/**
+ * Created by fomina.zhanna on 24.05.2017.
+ */
+public class TestFirefoxESR {
+    public WebDriver driver;
+
+    @Before
+    public void start(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        driver = new FirefoxDriver(new FirefoxBinary(new File("C:\\Users\\fomina.zhanna\\Mozilla Firefox ESR\\firefox.exe")), new FirefoxProfile(), caps);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+    }
+
+    @Test
+    public void esr(){
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[2]/button")).click();
+    }
+
+    @After
+    public void stop()
+    {
+        driver.quit();
+        driver = null;
+    }
+}
